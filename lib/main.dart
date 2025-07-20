3f0132644ab04f58905599af5c9e1dc7358800bc
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-20 19:40:38 +1000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+## 📄 lib/main.dart
+```dart
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  runApp(const TollMapApp());
+}
+
+class TollMapApp extends StatelessWidget {
+  const TollMapApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toll Map App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
+```
