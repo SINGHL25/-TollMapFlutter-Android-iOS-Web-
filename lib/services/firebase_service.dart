8f4555be79df3f4e4e48b11bea057448cbe24d55
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-20 19:42:42 +1000

    Update firebase_service.dart

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,1 +1,12 @@
+## 📄 lib/services/firebase_service.dart
+```dart
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/site_model.dart';
 
+class FirebaseService {
+  static Future<List<SiteModel>> getSites() async {
+    final snapshot = await FirebaseFirestore.instance.collection('toll_sites').get();
+    return snapshot.docs.map((doc) => SiteModel.fromFirestore(doc)).toList();
+  }
+}
+```
